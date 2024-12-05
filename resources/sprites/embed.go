fe321eac0180d836ee6197771bfc674cc32c05e6
Scot Bontrager <scot@indievisible.org>
2024-12-05 08:49:27 -0600

    add

---
+++ resources/sprites/embed.go
@@ -0,0 +1,43 @@
+package sprites
+
+import (
+	_ "embed"
+)
+
+var (
+	//go:embed artist.png
+	ArtistPNG []byte
+
+	//go:embed baby.png
+	BabyPNG []byte
+
+	//go:embed bathtime.png
+	BathtimePNG []byte
+
+	//go:embed confused.png
+	ConfusedPNG []byte
+
+	//go:embed happy.png
+	HappyPNG []byte
+
+	//go:embed indignent.png
+	IndignentPNG []byte
+
+	//go:embed love.png
+	LovePNG []byte
+
+	//go:embed mad.png
+	MadPNG []byte
+
+	//go:embed pinwheel.png
+	PinwheelPNG []byte
+
+	//go:embed spring.png
+	SpringPNG []byte
+
+	//go:embed swanmommy.png
+	SwanmommyPNG []byte
+
+	//go:embed teatime.png
+	TeatimePNG []byte
+)

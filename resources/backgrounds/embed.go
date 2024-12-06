c8c9385cfb68589ae08d5cc9f7530c6d7012943a
Scot Bontrager <scot@indievisible.org>
2024-12-05 21:50:33 -0600

    add

---
+++ resources/backgrounds/embed.go
@@ -0,0 +1,19 @@
+package backgrounds 
+
+import (
+	_ "embed"
+)
+
+var (
+	//go:embed flowersplash.png
+	Default []byte
+
+	//go:embed hummingbird_bg.png
+	Hummingbird []byte
+
+	//go:embed owleyes.png
+	Owleyes []byte
+
+	//go:embed owlmoon.png
+	Owlmoon []byte
+)

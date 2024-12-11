2bc0c7f2a564a62d60e27e6e787650d4e2ca43e2
Scot Bontrager <scot@indievisible.org>
2024-12-10 22:43:49 -0600

    draw buttons

--- cmd/ac/radio.go
+++ cmd/ac/radio.go
@@ -39,6 +39,10 @@ func (r *radiocontrol) getlabel() string {
 	return r.label
 }
 
+func (r *radiocontrol) setlabelimg(i *ebiten.Image) {
+	r.labelimg = i
+}
+
 var radiobuttons = []radiobutton{
 	{
 		sprite: getSprite("Tea Time"),
@@ -67,6 +71,21 @@ var radiobuttons = []radiobutton{
 	},
 }
 
+var radiocontrols = []radiocontrol{
+	{
+		sprite: getSprite("Tea Time"),
+		label:  "Pause",
+	},
+	{
+		sprite: getSprite("Swan Mommy"),
+		label:  "Play",
+	},
+	{
+		sprite: getSprite("Spring"),
+		label:  "Stop",
+	},
+}
+
 func radioDialog(g *Game) {
 	g.state = inRadio
 }
@@ -88,8 +107,6 @@ func (g *Game) drawRadioDialog(screen *ebiten.Image) {
 		if radiobuttons[idx].labelimg == nil {
 			genlabel(&(radiobuttons[idx]), color.RGBA{0x33, 0x33, 0x33, 0xee}, controlfont)
 		}
-		radiobuttons[idx].sprite.setScale(spriteScale)
-		radiobuttons[idx].sprite.draw(screen)
 
 		if radiobuttons[idx].labelloc.X == 0 {
 			b := radiobuttons[idx].sprite.image.Bounds()
@@ -118,6 +135,7 @@ func (g *Game) drawRadioControls(screen *ebiten.Image) {
 		return
 	}
 
+	// TODO: base this on sprite size not hardcoded values
 	grey := color.RGBA{0xaa, 0xaa, 0xaa, 0x99}
 	border := color.RGBA{0x66, 0x66, 0x66, 0x00}
 
@@ -126,6 +144,32 @@ func (g *Game) drawRadioControls(screen *ebiten.Image) {
 	vector.DrawFilledRect(screen, float32(borderwidth), float32(240), float32(screensize.X-(160)), float32(120), grey, false)
 	vector.StrokeRect(screen, float32(borderwidth), float32(240), float32(screensize.X-(160)), float32(120), float32(4), border, false)
 	vector.StrokeRect(screen, float32(borderwidth)*1.5, float32(250), float32(screensize.X-(180)), float32(100), float32(2), border, false)
+
+	x := 200
+
+	for idx := range radiocontrols {
+		radiocontrols[idx].sprite.setLocation(x, 270)
+		radiocontrols[idx].sprite.setScale(spriteScale)
+		radiocontrols[idx].sprite.draw(screen)
+
+		if radiocontrols[idx].labelimg == nil {
+			genlabel(&(radiocontrols[idx]), color.RGBA{0x33, 0x33, 0x33, 0xee}, controlfont)
+		}
+
+		if radiocontrols[idx].labelloc.X == 0 {
+			b := radiocontrols[idx].sprite.image.Bounds()
+			spritecenterx := int(float64(radiocontrols[idx].sprite.loc.X) + float64(b.Max.X)*spriteScale/2.0)
+			lb := radiocontrols[idx].labelimg.Bounds()
+			labelcenterx := lb.Max.X / 2
+			radiocontrols[idx].labelloc.X = spritecenterx - labelcenterx
+			radiocontrols[idx].labelloc.Y = radiocontrols[idx].sprite.loc.Y + int(float64(b.Max.Y)*spriteScale+4.0)
+		}
+
+		op := &ebiten.DrawImageOptions{}
+		op.GeoM.Translate(float64(radiocontrols[idx].labelloc.X), float64(radiocontrols[idx].labelloc.Y))
+		screen.DrawImage(radiocontrols[idx].labelimg, op)
+		x = x + 100
+	}
 }
 
 func (r radiobutton) startPlayer(g *Game) {
